4304b1c6ff5cb86411af07254332180a23206b9c
DevastatingRPG <devastatingrpg@gmail.com>
2023-11-09 11:21:33 +0530

    added 2c and 2b improved

---
+++ Task2/c/gg_2527_task_2c_output/task_2c.py
@@ -0,0 +1,291 @@
+'''
+*****************************************************************************************
+*
+*        		===============================================
+*           		GeoGuide(GG) Theme (eYRC 2023-24)
+*        		===============================================
+*
+*  This script is to implement Task 2C of GeoGuide(GG) Theme (eYRC 2023-24).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*
+*****************************************************************************************
+'''
+############################## FILL THE MANDATORY INFORMATION BELOW ###############################
+
+# Team ID:			2527
+# Author List:		Shubham Sarkar, Aditya Gajjar
+# Filename:			task_2c.py
+# Functions:	    [`classify_event(image)` ]
+###################################################################################################
+
+# IMPORTS (DO NOT CHANGE/REMOVE THESE IMPORTS)
+from sys import platform
+import numpy as np
+import subprocess
+import cv2 as cv       # OpenCV Library
+import shutil
+import ast
+import sys
+import os
+
+# Additional Imports
+'''
+You can import your required libraries here
+'''
+import copy
+import cv2
+from PIL import Image
+import torch
+from torchvision import transforms
+from torchvision.models import EfficientNet_V2_S_Weights, efficientnet_v2_s
+
+sys.path.append('..')
+from a.task_2a import detect_ArUco_details
+
+# DECLARING VARIABLES (DO NOT CHANGE/REMOVE THESE VARIABLES)
+arena_path = "arena.png"            # Path of generated arena image
+event_list = []
+detected_list = []
+
+# Declaring Variables
+'''
+You can delare the necessary variables here
+'''
+weights = EfficientNet_V2_S_Weights.DEFAULT
+torch.manual_seed(42)
+
+# EVENT NAMES
+'''
+We have already specified the event names that you should train your model with.
+DO NOT CHANGE THE BELOW EVENT NAMES IN ANY CASE
+If you have accidently created a different name for the event, you can create another 
+function to use the below shared event names wherever your event names are used.
+(Remember, the 'classify_event()' should always return the predefined event names)  
+'''
+combat = "combat"
+rehab = "humanitarianaid"
+military_vehicles = "militaryvehicles"
+fire = "fire"
+destroyed_building = "destroyedbuilding"
+
+# Extracting Events from Arena
+def arena_image(arena_path):            # NOTE: This function has already been done for you, don't make any changes in it.
+    ''' 
+	Purpose:
+	---
+	This function will take the path of the generated image as input and 
+    read the image specified by the path.
+	
+	Input Arguments:
+	---
+	`arena_path`: Generated image path i.e. arena_path (declared above) 	
+	
+	Returns:
+	---
+	`arena` : [ Numpy Array ]
+
+	Example call:
+	---
+	arena = arena_image(arena_path)
+	'''
+    '''
+    ADD YOUR CODE HERE
+    '''
+    frame = cv.imread(arena_path)
+    arena = cv.resize(frame, (700, 700))
+    return arena 
+
+def event_identification(arena):        # NOTE: You can tweak this function in case you need to give more inputs 
+    ''' 
+	Purpose:
+	---
+	This function will select the events on arena image and extract them as
+    separate images.
+	
+	Input Arguments:
+	---
+	`arena`: Image of arena detected by arena_image() 	
+	
+	Returns:
+	---
+	`event_list`,  : [ List ]
+                            event_list will store the extracted event images
+
+	Example call:
+	---
+	event_list = event_identification(arena)
+	'''
+    ArUco_details_dict, ArUco_corners = detect_ArUco_details(arena)
+    centers = copy.deepcopy(ArUco_details_dict)
+    corners = copy.deepcopy(ArUco_corners)
+    events = [
+        [corners[2][0], corners[2][3]], 
+        [corners[53][1], corners[0][3]],
+        [corners[54][1], corners[39][3]],     
+        [centers[5][0], corners[5][3]], 
+        [corners[22][3], centers[15][0]]   
+    ]
+
+    changes = (
+        (({0: -72, 1: 9}), ({1: 50, 0: -12 })),
+        (({0: 30, 1: 10}), ({1: 50, 0: -20})), 
+        (({0: 80, 1: 8}), ({0: -118, 1: -18})), 
+        (({0: -135, 1: 3}), ({1: 51, 0: -68})), 
+        (({1: 45, 0: 33}), ({1: -57, 0: -30})),    
+    )
+
+    for event in range(5):
+        for corner in range(2):
+            for axe in changes[event][corner]:
+                events[event][corner][axe] += changes[event][corner][axe]
+    
+    i=0
+    event_list=[]
+    for tl, br in events:
+        roi = arena[tl[1]:br[1], tl[0]:br[0]]
+        event_list.append(roi)
+        cv2.imwrite(f"events/{i}.jpg", roi)
+        i+=1
+
+    return event_list
+
+# Event Detection
+def classify_event(image):
+    ''' 
+	Purpose:
+	---
+	This function will load your trained model and classify the event from an image which is 
+    sent as an input.
+	
+	Input Arguments:
+	---
+	`image`: Image path sent by input file 	
+	
+	Returns:
+	---
+	`event` : [ String ]
+						  Detected event is returned in the form of a string
+
+	Example call:
+	---
+	event = classify_event(image_path)
+	'''
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = torch.load("../b/model.tf")
+    model.eval()
+    # image = np.expand_dims(image_resized, axis=0)
+    image_transform = transforms.Compose([
+        transforms.ToTensor(), # 2. Turn image values to between 0 & 1 
+        transforms.Resize((224, 224), antialias=False), 
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], # 3. A mean of [0.485, 0.456, 0.406] (across each colour channel)
+                         std=[0.229, 0.224, 0.225]) 
+    ])
+    
+    sr = cv2.dnn_superres.DnnSuperResImpl_create()
+    path = "EDSR_x4.pb"   
+    sr.readModel(path)   
+    sr.setModel("edsr",4)   
+    result = sr.upsample(image)
+    with torch.inference_mode():
+        # 6. Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+        transformed_image = image_transform(result).unsqueeze(dim=0)
+        # 7. Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(transformed_image.to(device))
+
+    # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # 9. Convert prediction probabilities -> prediction labels
+    pred = torch.argmax(target_image_pred_probs, dim=1)
+
+    # pred = model.predict(image)
+    class_names = ['combat', 'destroyedbuilding', 'fire', 'humanitarianaid', 'militaryvehicles']
+    event = class_names[pred]
+    return event
+
+# ADDITIONAL FUNCTIONS
+'''
+Although not required but if there are any additonal functions that you're using, you shall add them here. 
+'''
+###################################################################################################
+########################### DO NOT MAKE ANY CHANGES IN THE SCRIPT BELOW ###########################
+def classification(event_list):
+    for img_index in range(0,5):
+        img = event_list[img_index]
+        detected_event = classify_event(img)
+        print((img_index + 1), detected_event)
+        if detected_event == combat:
+            detected_list.append("combat")
+        if detected_event == rehab:
+            detected_list.append("rehab")
+        if detected_event == military_vehicles:
+            detected_list.append("militaryvehicles")
+        if detected_event == fire:
+            detected_list.append("fire")
+        if detected_event == destroyed_building:
+            detected_list.append("destroyedbuilding")
+    os.remove('arena.png')
+    return detected_list
+
+def detected_list_processing(detected_list):
+    try:
+        detected_events = open("detected_events.txt", "w")
+        detected_events.writelines(str(detected_list))
+    except Exception as e:
+        print("Error: ", e)
+
+def input_function():
+    if platform == "win32":
+        try:
+            subprocess.run("input.exe")
+        except Exception as e:
+            print("Error: ", e)
+    if platform == "linux":
+        try:
+            subprocess.run("./input")
+        except Exception as e:
+            print("Error: ", e)
+
+def output_function():
+    if platform == "win32":
+        try:
+            subprocess.run("output.exe")
+        except Exception as e:
+            print("Error: ", e)
+    if platform == "linux":
+        try:
+            subprocess.run("./output")
+        except Exception as e:
+            print("Error: ", e)
+
+###################################################################################################
+def main():
+    ##### Input #####
+    input_function()
+    #################
+
+    ##### Process #####
+    arena = arena_image(arena_path)
+    event_list = event_identification(arena)
+    detected_list = classification(event_list)
+    detected_list_processing(detected_list)
+    ###################
+
+    ##### Output #####
+    output_function()
+    ##################
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        if os.path.exists('arena.png'):
+            os.remove('arena.png')
+        if os.path.exists('detected_events.txt'):
+            os.remove('detected_events.txt')
+        sys.exit()
